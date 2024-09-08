1f5d742867939bb974384b096c338c5a30d7645e
Ahmad Khan <nuzulakhanahmadfirdausi@gmail.com>
2024-09-08 23:37:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/signuppage/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.signuppage
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.signuppage.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+    }
+}
