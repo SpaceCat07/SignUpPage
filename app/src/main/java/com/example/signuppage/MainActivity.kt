a58924b93b9c6c100c3af3323f441f2c1494bea6
Ahmad Khan <nuzulakhanahmadfirdausi@gmail.com>
2024-09-10 14:03:13 +0700

    menambahkan toast ketika di klik signup button

--- app/src/main/java/com/example/signuppage/MainActivity.kt
+++ app/src/main/java/com/example/signuppage/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.signuppage
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,5 +14,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        binding.btnSign.setOnClickListener {
+            val email = binding.inputEmail.text.toString()
+            val fullName = binding.inputFullName.text.toString()
+            val userName = binding.inputUsername.text.toString()
+            val password = binding.inputPassword.text.toString()
+            val hasil = "Email is $email, Fullname is $fullName, Username is $userName"
+
+            if (email != "" && fullName != "" && userName != "" && password != "") {
+                Toast.makeText(this, hasil, Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Please fill all section above", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
